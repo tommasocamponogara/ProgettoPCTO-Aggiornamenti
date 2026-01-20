8dfb6f33e6ce0ddee2097a94dfddb2d3abfbc83f
E-S-P-O <francyespo07@gmail.com>
2026-01-20 16:03:58 +0100

    topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom'
+import { Topbar } from './componenti/Topbar'
 export default function App() {
-  return <h1>CIao</h1>
+  return (
+    <span>
+      <Topbar />
+      <Link to="/line">kajsdhf</Link>
+    </span>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import App from './App'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
