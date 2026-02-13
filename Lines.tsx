3ba816ab1ea58292f67c42768870532886de38db
JasSainii <jasdeepsing2006@gmail.com>
2026-02-13 17:44:48 +0100

    Add files via upload
    fFile modificati + fix database

---
+++ Lines.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import { Sidebar } from '../componenti/Sidebar'
+import { TableLines } from '../componenti/TableLines'
+import { Topbar } from '../componenti/Topbar'
+import type { Line } from '../Types/Type'
+import { getLines } from '../utils/api'
+
+export default function Lines() {
+  // Stato per le linee (anche se qui viene usato solo per il trigger del caricamento)
+  const [, setLines] = useState<Line[]>([])
+
+  useEffect(() => {
+    // Caricamento iniziale dei dati delle linee
+    getLines().then((lines) => setLines(lines))
+  }, [])
+
+  return (
+    <>
+      <Sidebar />
+      <Topbar />
+      {/* Componente tabella che visualizzerà i dati delle linee */}
+      <TableLines />
+    </>
+  )
+}
---
+++ TableLines.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react'
+import { getLines } from '../utils/api'
+import { useNavigate } from 'react-router-dom'
+import type { Line } from '../Types/Type'
+import { getDetailsLineMachine } from '../utils/getDetailsLineMachine'
+import { AddLines } from '../pages/AddLines'
+
+// Componente che mostra una tabella con le linee di produzione (cioe quando l'utente clicca su linee nella sidebar, viene mostrata una tabella con tutte le linee presenti nel database)
+
+export function TableLines() {
+  const navigate = useNavigate()
+  // Crea un array vuoto di oggetti di tipo Line e un funzione per aggiornarlo
+  const [lines, setLines] = useState<Line[]>([])
+
+  // Al caricamento recupera le linee per avere i dati aggiornati dal database
+  useEffect(() => {
+    // Quando il componente viene montato, chiama la funzione getLines presente in API.ts, per recuperare le linee di produzione dal database, per poi aggiornare lo stato del componente
+    getLines().then((lines) => setLines(lines))
+  }, [])
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-800 w-full font-mono">
+      <div className="max-h-[70vh] overflow-y-auto w-3/4 max-w-5xl rounded-lg shadow-lg shadow-black/40">
+        <button
+          onClick={() => navigate('/dashboard/lines/AddLines')}
+          className="px-4 py-2 bg-amber-500 text-slate-900 rounded-lg hover:bg-amber-400 transition-colors"
+        >
+          Aggiungi Linea
+        </button>
+        <table className="w-full border-collapse">
+          <thead className="bg-amber-700 text-slate-900 sticky top-0 z-10">
+            <tr>
+              <th className="px-6 py-4 text-center text-lg font-semibold uppercase tracking-wider">
+                ID#
+              </th>
+              <th className="px-6 py-4 text-center text-lg font-semibold uppercase tracking-wider">
+                Linea
+              </th>
+              <th className="px-6 py-4 text-center text-lg font-semibold uppercase tracking-wider">
+                Stato
+              </th>
+              <th className="px-6 py-4 text-center text-lg font-semibold uppercase tracking-wider">
+                N. Macchine
+              </th>
+              <th className="px-6 py-4 text-center text-lg font-semibold uppercase tracking-wider">
+                Descrizione
+              </th>
+            </tr>
+          </thead>
+          \
+          <tbody className="bg-slate-900 text-slate-200 divide-y divide-slate-700 text-center">
+            {lines.map((line) => {
+              // Per ogni linea di produzione recupera il numero di allarmi attivi per quella linea (grazie alla funzione numbersOfAlarms definita in getDetailsLineMachine)
+              // Calcola il numero di allarmi attivi per ogni singola linea
+              const { numbersOfAlarms } = getDetailsLineMachine()
+              const lineNumberAlarms = numbersOfAlarms(line.machines)
+              return (
+                <tr key={line.id} className="hover:bg-slate-800 transition-colors">
+                  <td
+                    className="px-6 py-4 hover:cursor-pointer hover:text-amber-400"
+                    onClick={() => navigate(`${line.id}`)}
+                  >
+                    {'#' + line.id}
+                  </td>
+                  <td className="px-6 py-4">{line.name}</td>
+                  <td className="px-6 py-4">
+                    {/* Badge colorato in base allo stato della linea */}
+                    <span
+                      className={`inline-flex items-center justify-center w-25 h-6 rounded-full text-xs font-semibold ${
+                        line.status === 'positive'
+                          ? 'bg-green-500 text-slate-900'
+                          : line.status === 'wait'
+                            ? 'bg-amber-500 text-slate-900'
+                            : 'bg-red-500 text-slate-900'
+                      }`}
+                    >
+                      {line.status === 'positive'
+                        ? `Attiva (${lineNumberAlarms})`
+                        : line.status === 'wait'
+                          ? `Attesa (${lineNumberAlarms})`
+                          : `Bloccata (${lineNumberAlarms})`}
+                    </span>
+                  </td>
+                  <td className="px-6 py-4">{line.machines.length}</td>
+                  <td className="px-6 py-4">{line.description}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
